9008772e861dfaef99480ca610f5e926ac8686bf
Meigo <73817505+meigoc@users.noreply.github.com>
2025-02-09 23:08:56 +0300

    Update MPD.java

--- MPD.java
+++ MPD.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class MPD {
-//9999нh//д авпапoз444444lдр33333333333222
+//9999нh//д авпапoз444444lдр3333333333322sdf2
     public static void createMPDArchive(String sourceFile, String archiveFile) {
         try (FileOutputStream fos = new FileOutputStream(archiveFile);
              BufferedOutputStream bos = new BufferedOutputStream(fos);
